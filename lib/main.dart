0904a8f30be5698b2769603fafbeed28371a8930
vikeshkumar <vikeshvee@gmail.com>
2021-05-21 09:36:01 +0530

    day 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:test2/pages/home_page.dart';
+import 'package:test2/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,7 +11,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(child: Center(child: Text("Hellow world"))),
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+          primarySwatch: Colors.deepPurple,
+          fontFamily: GoogleFonts.lato().fontFamily),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      // home: HomePage(),
+      // initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/login": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+      },
     );
   }
 }
