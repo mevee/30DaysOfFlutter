25ef687dfba95c893149697ab56d1a5f70dc9aad
vikeshkumar <vikeshvee@gmail.com>
2021-05-24 08:58:42 +0530

    animated Container

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,10 +7,15 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        // automaticallyImplyLeading: false,
         title: Text("Catelog App"),
       ),
-      body: Center(child: Text("Wellcome to 30 days of Flutter")),
-      drawer: Drawer(),
+      body: Center(
+          child: Text(
+        "Wellcome to 30 days of Flutter",
+        style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+      )),
+      // drawer: Drawer(),
     );
   }
 }
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:test2/util/routes.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _pageTitle = "";
+  bool isChanged = false;
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
@@ -20,7 +32,7 @@ class LoginPage extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.all(8.0),
               child: Text(
-                "Welcome",
+                "$_pageTitle",
                 style: TextStyle(
                     color: Colors.black,
                     fontSize: 28,
@@ -38,6 +50,11 @@ class LoginPage extends StatelessWidget {
               child: Column(
                 children: [
                   TextFormField(
+                    onChanged: (value) {
+                      setState(() {
+                        _pageTitle = "Wellcome $value";
+                      });
+                    },
                     decoration: InputDecoration(
                       hintText: "Enter email",
                       labelText: "Email",
@@ -56,13 +73,43 @@ class LoginPage extends StatelessWidget {
             SizedBox(
               height: 40.0,
             ),
-            ElevatedButton(
-              onPressed: () {
+            InkWell(
+              onTap: () async{
+                isChanged = true;
+                setState(() {});
+
+                await Future.delayed(Duration(seconds: 1));
                 Navigator.pushNamed(context, MyRoutes.home);
               },
-              child: Text("Login"),
-              style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+              child: AnimatedContainer(
+                duration: Duration(seconds: 1),
+                alignment: Alignment.center,
+                width: isChanged ? 50 : 150,
+                height: 50,
+                child: isChanged
+                    ? Icon(Icons.done, color: Colors.white)
+                    : Text(
+                        "Login",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                      ),
+                decoration: BoxDecoration(
+                    color: Colors.deepPurple,
+                    // shape: isChanged ? BoxShape.circle : BoxShape.circle,
+                    borderRadius: BorderRadius.circular(isChanged ? 50 : 8)),
+              ),
             ),
+
+            // ElevatedButton(
+            //   onPressed: () {
+            //     Navigator.pushNamed(context, MyRoutes.home);
+            //     // Navigator.pop(context);
+            //   },
+            //   child: Text("Login"),
+            //   style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+            // ),
           ],
         ),
       ),
